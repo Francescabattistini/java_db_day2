07efdf7296ce6a092c4a17f989c7f846ace0d209
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-10-08 17:27:08 +0200

    Update Application.java

--- src/main/java/francescaBattistini/Application.java
+++ src/main/java/francescaBattistini/Application.java
@@ -2,9 +2,12 @@ package francescaBattistini;
 
 import francescaBattistini.dao.EventoDAO;
 import francescaBattistini.entities.Evento;
+import francescaBattistini.entities.TipoEvento;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
+import java.time.LocalDate;
 
 public class Application {
     //1.secondo step dopo persistence
@@ -21,8 +24,8 @@ private static final EntityManagerFactory emf = Persistence.createEntityManagerF
 //5. segno la dao
      EventoDAO ed = new EventoDAO(em);
      //6. costruisco la colonna
-     
-
+     Evento evento1 = new Evento("Carlos Santana", LocalDate.of(2024,12, 30), TipoEvento.PRIVATO,10);
+     ed.save(evento1);
 
      //3. Best practices, alla fine di tutto sempre bene chiudere le risorse aperte
      em.close();
